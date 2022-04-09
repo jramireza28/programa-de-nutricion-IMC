c52b8f8e77d24ccdc8f51fab41850a701afed6a9
jramireza28 <jairo1234*>
2022-04-08 21:32:37 -0600

    codigo

---
+++ programa de nutricion IMC/Program.cs
@@ -0,0 +1,7 @@
+﻿using programa_de_nutricion_IMC;
+
+Imc t = new Imc();
+
+Console.WriteLine(t.prueba());//codigo
+
+Console.WriteLine("\nLa salud es una Relación entre Tú y tu Cuerpo PorFavor Cuidala y Protegete");
